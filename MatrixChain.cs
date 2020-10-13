d7eaed22cea4e88e2a38c2ce98c8dab51f5bb917
Masiu games <masiugames@gmail.com>
2020-10-13 20:27:04 +0200

    Dodaj pliki projektów.

---
+++ MatrixChain.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    class MatrixChain
+    {
+        Random rand = new Random();
+
+        public int x, y, maxY;
+        int[] matrixNumbers = new int[5];
+
+        public MatrixChain(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            maxY = y + 7;
+
+            for(int i = 0; i < matrixNumbers.Length; i++)
+            {
+                matrixNumbers[i] = rand.Next(0, 2);
+            }
+        }
+
+        public void updateMatrix()
+        {
+            maxY = y + 7;
+            Console.SetCursorPosition(x, y);
+            Console.Write(" ");
+            y = y + 1;
+
+            for (int i = 0; i < matrixNumbers.Length; i++)
+            {
+                matrixNumbers[i] = rand.Next(0, 2);
+            }
+
+            printMatrix();
+        }
+
+        void printMatrix()
+        {
+            for (int i = 0; i < matrixNumbers.Length; i++)
+            {
+                 Console.SetCursorPosition(x, y + i);
+                 Console.Write(matrixNumbers[i]);
+            }
+        }
+
+        public void unprintMatrix()
+        {
+            for (int i = 0; i < matrixNumbers.Length; i++) {
+                Console.SetCursorPosition(x, y + i);
+                Console.Write(" ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matrix
+{
+    class Program
+    {
+        static List<MatrixChain> matrixChains = new List<MatrixChain>();
+        static Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                while(matrixChains.Count<200) {
+                    MatrixChain chain = new MatrixChain(rand.Next(0, Console.WindowWidth), rand.Next(0, Console.WindowHeight-5));
+                    matrixChains.Add(chain);
+                }
+
+                for (int i = 0; i < matrixChains.Count; i++)
+                {
+                    matrixChains[i].updateMatrix();
+                    if(matrixChains[i].maxY > Console.WindowHeight)
+                    {
+                        matrixChains[i].unprintMatrix();
+                        matrixChains[i].y = 0;
+                        
+                    }
+                    System.Threading.Thread.Sleep(0);
+                } 
+            }
+        }
+    }
+}
